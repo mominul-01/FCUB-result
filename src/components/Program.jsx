010373fafd17f8d44e1520d0a1c4d11a4488b5f7
sahosridoy <sahosmia.webdev@gmail.com>
2023-08-07 00:30:31 +0600

    subject show without total creadit

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -1,44 +1,37 @@
-import React, { useState } from 'react';
-import data from '../../public/subject.json' 
+import React, { useState } from "react";
+import data from "../../public/subject.json";
 
-const Program = () => {
-  const [selectedProgram, setSelectedProgram] = useState('');
+const Program = ({ onDepartmentData }) => {
+  const [selectedDepartment, setSelectedDepartment] = useState("");
 
   const handleChange = (e) => {
-    const selectedProgramValue = e.target.value;
-    setSelectedProgram(selectedProgramValue);
-    console.log(`Selected Program: ${selectedProgramValue}`);
+    const selectedDepartmentValue = e.target.value;
+    setSelectedDepartment(selectedDepartmentValue);
+    onDepartmentData(e.target.value);
+
+    // console.log(`Selected Department: ${selectedDepartmentValue}`);
   };
 
-  console.log(data);
-
-
   return (
-    <div className=' p-5 '>
-      <label htmlFor="program-dropdown" className='text-2xl  p-3'>Program Name:</label>
-          <select id="program-dropdown" value={selectedProgram} onChange={handleChange}
-            className=' text-lg rounded-lg px-5  mx-3 py-3'
-          >
-        <option value="">Select Program</option>
-        <option value="BBA">BBA</option>
-        {/* {
-          data.departments.map((item,i) => (
-            <option key={i} value="BBA">{ item}</option>
-
-          ))
-        } */}
-        <option value="LLB">LLB</option>
-        <option value="Sociology">Sociology</option>
-        <option value="English">English</option>
-        <option value="CSE">CSE</option>
-        <option value="Agriculture">Agriculture</option>
-        <option value="EEE">EEE</option>
-        <option value="BPH">BPH</option>
+    <div className=" p-5 ">
+      <label htmlFor="department-dropdown" className="text-2xl  p-3">
+        Department Name:
+      </label>
+      <select
+        id="department-dropdown"
+        value={selectedDepartment}
+        onChange={handleChange}
+        className=" text-lg rounded-lg px-5  mx-3 py-3"
+      >
+        <option value="">Select Department</option>
+        {Object.keys(data.departments).map((item, i) => (
+          <option key={i} value={item}>
+            {item}
+          </option>
+        ))}
       </select>
 
-      {
-        data.map
-      }
+      {data.map}
     </div>
   );
 };
